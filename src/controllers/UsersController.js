7552c71456b988d6e99d41433197ce3812a3a98b
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-06-15 23:56:36 -0300

    fix: UserController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -6,7 +6,10 @@ export class UserController {
   async create (req, res) {
     const { name, email, password } = req.body
     const database = await dbConnect()
-    const checkUser = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+    const checkUser = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
 
     if (checkUser) {
       throw new AppError('This email is already registered', 409)
@@ -15,9 +18,18 @@ export class UserController {
     const hashedPassword = await bcryptjs.hash(password, 8)
 
     await database.run(
-      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword]
+      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+      [name, email, hashedPassword]
     )
 
     res.status(201).json()
+  }
+
+  async update (req, res) {
+    const { name, email, password, avatar } = req.body
+    const { id } = req.params
+
+    const database = await dbConnect()
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
   }
 }
